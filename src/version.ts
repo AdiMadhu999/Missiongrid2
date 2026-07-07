9fd90d0780e1fc3b052a69df5edc097093727ba3
Mission Selection <missionselectionofficial999@gmail.com>
2026-07-07 23:49:43 +0000

    fix(resources): replace all corrupted splash screens with valid high-quality PNGs

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const APP_VERSION = "1.0.53";
-export const GIT_COMMIT = "9642d52";
-export const BUILD_TIMESTAMP = "7/7/2026, 10:42:00 PM UTC";
+export const GIT_COMMIT = "e0d3bcf";
+export const BUILD_TIMESTAMP = "7/7/2026, 11:43:29 PM UTC";
