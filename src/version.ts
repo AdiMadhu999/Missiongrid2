1f190d0e8acb4af42f6386a8aaf79a2bab3573e4
Mission Selection <missionselectionofficial999@gmail.com>
2026-07-07 23:23:06 +0000

    fix: replace corrupt gradle-wrapper.jar and add new app icons

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const APP_VERSION = "1.0.53";
-export const GIT_COMMIT = "445af86";
-export const BUILD_TIMESTAMP = "7/7/2026, 10:06:22 PM UTC";
+export const GIT_COMMIT = "9642d52";
+export const BUILD_TIMESTAMP = "7/7/2026, 10:42:00 PM UTC";
