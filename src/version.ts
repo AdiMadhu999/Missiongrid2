9117cc29e788c1a20815f2723e0282b06ed3b223
MissionSelectionAssistant <missionselectionofficial999@gmail.com>
2026-07-08 23:27:31 +0000

    build: resolve AAPT2 image compilation failure by regenerating native launcher icons and splash screens

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const APP_VERSION = "1.0.53";
-export const GIT_COMMIT = "058eb68";
-export const BUILD_TIMESTAMP = "7/8/2026, 11:17:56 PM UTC";
+export const GIT_COMMIT = "73abac8";
+export const BUILD_TIMESTAMP = "7/8/2026, 11:26:44 PM UTC";
