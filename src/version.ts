3fc6e612497952f3d2c33f5dad515168acbea474
MissionSelectionAssistant <missionselectionofficial999@gmail.com>
2026-07-08 23:28:04 +0000

    build: update version and build metadata matching latest commit

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const APP_VERSION = "1.0.53";
-export const GIT_COMMIT = "73abac8";
-export const BUILD_TIMESTAMP = "7/8/2026, 11:26:44 PM UTC";
+export const GIT_COMMIT = "9117cc2";
+export const BUILD_TIMESTAMP = "7/8/2026, 11:28:00 PM UTC";
