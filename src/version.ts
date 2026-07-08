60f8268b10c10ad4335df0695886373b7973cbae
MissionSelectionAssistant <missionselectionofficial999@gmail.com>
2026-07-08 23:38:21 +0000

    build: configure .gitattributes binary status for JAR files and modernize Firebase hosting merge workflow Gradle setup to avoid corrupt gradle-wrapper.jar on GitHub

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "1.0.53";
-export const GIT_COMMIT = "9117cc2";
-export const BUILD_TIMESTAMP = "7/8/2026, 11:28:00 PM UTC";
+export const APP_VERSION = "1.0.54";
+export const GIT_COMMIT = "pending";
+export const BUILD_TIMESTAMP = "7/8/2026, 11:38:00 PM UTC";
