ce95d3d7e49a7eb1ca8ed53403eeefcc4786d07c
Mission Selection <missionselectionofficial999@gmail.com>
2026-07-08 02:10:39 +0000

    Fix: Update fetch interceptor and api routing for web/APK environments

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const APP_VERSION = "1.0.53";
-export const GIT_COMMIT = "357af79";
-export const BUILD_TIMESTAMP = "7/8/2026, 2:05:27 AM UTC";
+export const GIT_COMMIT = "18eb7bc";
+export const BUILD_TIMESTAMP = "7/8/2026, 2:09:15 AM UTC";
