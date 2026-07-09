b00c89029e3cb6f82f295a4b94175b5d75e47066
Adi Madhu <missionselectionofficial999@gmail.com>
2026-07-09 13:20:05 +0000

    chore: bump version to 1.0.60 and clear build artifacts for clean APK generation

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "1.0.59";
-export const GIT_COMMIT = "12df480";
-export const BUILD_TIMESTAMP = "7/9/2026, 1:18:19 PM UTC";
+export const APP_VERSION = "1.0.60";
+export const GIT_COMMIT = "b904ba9";
+export const BUILD_TIMESTAMP = "7/9/2026, 1:19:52 PM UTC";
