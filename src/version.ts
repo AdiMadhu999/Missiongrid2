80807ff2b14e9e0ddc84c37fb5241736f3c904e4
Adi Madhu <missionselectionofficial999@gmail.com>
2026-07-09 03:14:38 +0000

    build: update app version build hash after asset recovery

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const APP_VERSION = "1.0.53";
-export const GIT_COMMIT = "498641e";
-export const BUILD_TIMESTAMP = "7/9/2026, 3:03:16 AM UTC";
+export const GIT_COMMIT = "6c74948";
+export const BUILD_TIMESTAMP = "7/9/2026, 3:13:40 AM UTC";
