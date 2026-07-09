b904ba96db1bb42dba95e725dce5bd0125b8ecfe
Adi Madhu <missionselectionofficial999@gmail.com>
2026-07-09 13:18:24 +0000

    feat: bump version to 1.0.59 to trigger fresh APK build

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "1.0.58";
-export const GIT_COMMIT = "61848c4";
-export const BUILD_TIMESTAMP = "7/9/2026, 12:25:23 PM UTC";
+export const APP_VERSION = "1.0.59";
+export const GIT_COMMIT = "12df480";
+export const BUILD_TIMESTAMP = "7/9/2026, 1:18:19 PM UTC";
