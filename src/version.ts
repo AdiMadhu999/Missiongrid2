f6abf681f941ffeb51cf2093290d50b955985384
Adi Madhu <missionselectionofficial999@gmail.com>
2026-07-10 02:54:35 +0000

    fix(android): replace corrupted PNGs with valid images to fix AAPT compilation error

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const APP_VERSION = "1.0.60";
-export const GIT_COMMIT = "0187e5b";
-export const BUILD_TIMESTAMP = "7/10/2026, 2:47:26 AM UTC";
+export const GIT_COMMIT = "761be13";
+export const BUILD_TIMESTAMP = "7/10/2026, 2:49:30 AM UTC";
